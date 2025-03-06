e00fa2c7cac7ee15f169c22ab5b9a60adc02e1b8
Enock <enock@influencer.lan>
2025-03-06 09:20:44 +0300

    made some additions

---
+++ Python/Variables_Primitive_Data_Types/primitive_data_types.py
@@ -0,0 +1,33 @@
+#Numbers
+
+#1. Integer
+# age = 12
+# print(type(age))
+
+    #2. Float
+# height =1.5
+# print(type(height))
+
+# sum = age + height
+# print(type(sum))
+
+#3. Boolean
+
+# is_student = False
+# print(is_student)
+
+#4 String
+
+# student_name ="Enock"
+# teacher_name ='Ben'
+# student_name = "Kipngeno"
+long_string ="neighbour"
+# class_rep = "Rop"
+# z = long_string[-1]
+# print(z)
+# 
+# z = student_name[-3]
+# print(z)
+print(long_string[0:3])
+print(long_string[-3:])
+print(long_string[:4])
---
+++ Python/Variables_Primitive_Data_Types/variables.py
@@ -0,0 +1,60 @@
+#A Variable is a container for storing data values.
+
+#NAMING CONVENTIONS.
+
+#A variable name must start with a letter or the underscore character.
+# student = "Enock"
+# print(student)
+# _dob = "5-03-2025"
+# print(_dob)
+#A variable name cannot start with a number.
+# 3student = "Enock"
+#A variable name can only contain alpha-numeric characters and underscores (A-Z 0-9 and _).
+# student1 ="Kirui"
+# print(student1)
+# student_name = "Kipngeno"
+# print(student_name)
+#Variable names are case-sensitive(age Age and AGE are three different variables).
+# age = 25
+# print("age:" , age)
+# Age = 30
+# age = 25
+# print("Age:" , Age)
+
+#Variable names cannot contain spaces.
+# student name ="Enock"
+# print( student name)
+#Variable names should be descriptive and meaningful (age is better than a).
+# is_student = True
+# print(is_student)
+# student_ID=2345
+# print(student_ID)
+#Variable names cannot be a reserved keyword in python.
+# global = "world"
+
+    #Standard Naming Conventions
+
+    #1. Camel Case
+
+# studentName = "Enock"
+# print(studentName)
+
+    #2. Pascal Case
+
+# StudentName = "Rop"
+# print(StudentName)
+
+    #Snake Case
+
+# student_name ="Dorothy"
+# print(student_name)
+
+#Variable can be overwritten/updated.
+# student_ID = 10
+# print(student_ID)
+# student_ID = 20
+# print(student_ID)
+
+
+
+
