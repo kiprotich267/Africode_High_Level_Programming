716bb6f63690778e112e4112bfc573c40c6f6247
Enock <enock@influencer.lan>
2025-03-20 13:08:03 +0300

    added logical operators file

---
+++ Python/logical_operators/logical_operators.py
@@ -0,0 +1,13 @@
+# AND : true when both operands are true
+
+is_student = False
+is_present = True
+
+print(is_student | is_present)
+
+#OR : true when atleast one operand is true
+# print(is_student or is_present)
+
+#NOT: true when operand is false - it negate the operand
+#print(not is_student)
+#print(not is_present)
