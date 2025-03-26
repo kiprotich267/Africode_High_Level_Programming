99f022d58bdebf3fc164b87c3994e1e7df308696
Enock <enock@influencer.lan>
2025-03-26 15:07:11 +0300

    added while and for loop

---
+++ Python/while_loop/while_loop.py
@@ -0,0 +1,30 @@
+import time
+
+counter = 0
+
+# while counter <= 5:
+#       counter += 1
+#       print(f"loop number:",counter)
+
+# username = input("Enter your username : ").lower()
+
+# while username != "Q".lower():
+#     print(f"your username is {username}")
+#     username = input("Enter your username:").lower()
+
+# x = 0
+
+
+# while x < 1:
+#     print("x" * 20)
+#     time.sleep(0.1)
+# i = 1
+# while i <= 5:
+#     j = 1
+#     while j <= 5:
+#         print(f"{i} x {j} = {i*j}")
+#         j += 1
+#     print("__________________________")
+#     i += 1
+  
+
