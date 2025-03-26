47a9e8ad47d64ec1491b90c45ed2b0b000257278
Enock <enock@influencer.lan>
2025-03-26 15:08:57 +0300

    added primitive data types

---
+++ Python/Variables_Primitive_Data_Types/input.py
@@ -0,0 +1,19 @@
+#str
+# name = input("what is your name")
+# print('my name is' + name)
+
+# age = input('how old are you')
+# print('i am'+age + 'years old')
+
+#int
+
+# boys = int(input('boys'))
+# girls = int(input('girls'))
+# result = boys + girls 
+# print(result)
+
+print('what is your name?')
+name = input()
+print('how old are you?')
+age = input()
+print('my name is ' + name + ' and i am ' + age + ' years old')
